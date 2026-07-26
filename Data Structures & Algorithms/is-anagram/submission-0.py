d9012620eb6a47b84f5e7309b90f0f71a36e0b26
Karthik <83576946+Karthik8150@users.noreply.github.com>
2026-07-26 13:45:29 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = Counter(s)
+        t_map = Counter(t)
+
+        if s_map == t_map:
+            return True
+        return False
+        
